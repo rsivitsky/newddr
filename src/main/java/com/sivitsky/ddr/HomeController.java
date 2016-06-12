6a6dbcfea329154ec0685875421ffc1ad8f193ad
ruslansivitsky <rsivitsky@gmail.com>
2016-06-12 11:07:39 +0300

    destroy session, delete temp user and user's cart from database

--- src/main/java/com/sivitsky/ddr/HomeController.java
+++ src/main/java/com/sivitsky/ddr/HomeController.java
@@ -81,8 +81,8 @@ public class HomeController {
                             Cart cart, User user) {
 
         HttpSession session = httpRequest.getSession(true);
-        int timeLive = 6; //ten minutes
-        session.setMaxInactiveInterval(timeLive);
+        //int timeLive = 6; //ten minutes
+        //session.setMaxInactiveInterval(timeLive);
         session.setAttribute("price_from", (price_from == null) ? 0 : Float.parseFloat(price_from));
         session.setAttribute("price_to", (price_to == null) ? 0 : Float.parseFloat(price_to));
 
--- src/main/java/com/sivitsky/ddr/service/HttpSessListener.java
+++ src/main/java/com/sivitsky/ddr/service/HttpSessListener.java
@@ -1,31 +1,36 @@
 package com.sivitsky.ddr.service;
 
 import com.sivitsky.ddr.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 public class HttpSessListener implements HttpSessionListener {
-    @Autowired
+
     private CartService cartService;
-    @Autowired
     private UserService userService;
 
     public void sessionCreated(HttpSessionEvent e) {
-        System.out.println("!session created");
+        if (userService == null || cartService == null) {
+            obtainUserAndCartService(e);
+        }
+        System.out.println("session created, beans obtained!");
     }
 
     public void sessionDestroyed(HttpSessionEvent e) {
+        User userForDelete = (User) e.getSession().getAttribute("anonym");
+        cartService.removeCart(cartService.getCartByUser(userForDelete));
+        userService.removeUser(userForDelete);
+        System.out.println("anonym!!!destroy");
+    }
+
+    private void obtainUserAndCartService(HttpSessionEvent e) {
         HttpSession session = e.getSession();
-        if (session.getAttribute("anonym") != null) {
-            cartService.removeCart(cartService.getCartByUser((User) session.getAttribute("anonym")));
-            userService.removeUser((User) session.getAttribute("anonym"));
-            session.removeAttribute("anonym");
-            System.out.println("anonym!!!destroy");
-        } else {
-            System.out.println("!!!destroy");
-        }
+        WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(session.getServletContext());
+        userService = (UserService) ctx.getBean("userServiceImpl");
+        cartService = (CartService) ctx.getBean("cartServiceImpl");
     }
 }
